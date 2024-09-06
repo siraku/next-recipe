93b31cd1676b333b921ac38cfbe2f0e9a880b76b
Shi <le.shi@accenture.com>
2024-09-06 14:53:47 +0900

    modify detail page

--- src/app/recipe-detail/[id]/page.js
+++ src/app/recipe-detail/[id]/page.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-const Detail = () => {
-  return <div>Detail</div>;
+const getRecipeDetail= async (id)=>{
+  try {
+     const apiResponse= await fetch(`https://dummyjson.com/recipes/${id}`)
+     const result= await apiResponse.json()
+    //  console.log(result)
+     return result
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const Detail = async ({params}) => {
+  const recepi= await getRecipeDetail(params.id)
+  return (<div> <div className="flex"> <img src={recepi.image} alt={recepi.name} className="h-full w-full object-cover object-top"/> <div><div>Recipe Name: {recepi.name}</div><div>cuisine: {recepi.cuisine}</div> <ul></ul> </div></div></div>)
 };
 
 export default Detail;
